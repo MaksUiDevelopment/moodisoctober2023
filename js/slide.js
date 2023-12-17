de4c1798e079f6fbf49552820ba6f4ed9c45cf8c
MaksUiDevelopment <104228603+MaksUiDevelopment@users.noreply.github.com>
2023-12-17 18:23:13 +0300

    1-2-9

---
+++ js/slide.js
@@ -0,0 +1,10 @@
+$(function () {
+   /* Slider */
+   $("[data-slider]").slick({
+      dots: true,
+      infinite: true,
+      speed: 1300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+   });
+});
